f01a889d53c92e9d82d442600e28a5fa309ca983
Thiago Oliveira <thiagooliveira745543@gmail.com>
2026-04-24 10:31:31 -0300

    Update detalhes-do-produto.php
    sim

--- detalhes-do-produto.php
+++ detalhes-do-produto.php
@@ -24,7 +24,7 @@ $produto = $_SESSION['produtos'][$index];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Deatlhes do Produto</title>
+    <title>Detalhes do Produto</title>
 </head>
 <body>
     <?php
